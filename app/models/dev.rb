ee5974ab4338a84ac349d579d505e30bdb35f4cf
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-08-17 10:55:43 -0400

    commit tree cdfb7440eaef79b3b5c0075e653c20fee49c99bb

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, :through => :freebies
+
+    def received_one?(item_name)
+
+        arr = self.freebies
+        c = arr.where(item_name: item_name)
+        d = c.empty?
+        d ? false : true
+
+    end
+
+    def give_away(dev, freebie)
+        received_one?(freebie.item_name) ? freebie.update(dev_id: dev.id) : "Freebie does not belong to dev trying to give it away"
+    end
 
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,12 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :company
+    belongs_to :dev
+
+    def print_details
+        #{insert dev's name} owns a {insert
+        #freebie's item_name} from {insert company's name}
+
+        puts "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+    end
 
 end
