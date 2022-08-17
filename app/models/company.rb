cddd50d74c5f427b319cda47661f59566efcd929
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-08-17 10:56:13 -0400

    completed

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, :through => :freebies
+
+    def give_freebie(dev, item_name, value)
+        c = Freebie.new(item_name: item_name, value: value, company_id: self.id, dev_id: dev.id)
+        c.save
+    end
+
+    def oldest_company
+        oldest = Company.minimum(:founding_year)
+        return Company.where(founding_year: oldest)
+    end
 
 end
